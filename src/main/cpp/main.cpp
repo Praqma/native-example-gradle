1593b19bc52cffe4f48f3450df7d5c00f3125685
Leonard Lee <leonard.lee@praqma.com>
2017-09-28 13:41:27 +0200

    Remove old naming references.

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,12 +1,10 @@
 #include "greeter.hpp"
-#include "timey.h"
 #include "rectangley.h"
 #include "triangley.h"
 #include <iostream>
-#include <string>
 
 int main(int argc, char* argv[]) {
-    Greeter g("Gradle User");
+    Greeter g("Human Folk");
     g.greet();
     std::cout << "Rectangley has " << Rectangley(4, 5).getArea() << " area units." << std::endl;
     std::cout << "Triangley has " << Triangley(4, 5).getArea() << " area units." << std::endl;
